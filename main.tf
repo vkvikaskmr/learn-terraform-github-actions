6485bad6ccd432db8f14530bd8df89b232608b5b
vkvikaskmr <vkvikaskmr@gmail.com>
2023-07-12 18:02:46 +0530

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "ACG-Terraform-Labs-vik"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "lab-migrate-state-test"
     }
   }
 }
